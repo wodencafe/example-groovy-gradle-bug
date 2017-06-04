4d8b418929d0790732edad969fdb0432a815af28
wodencafe <wodencafe@gmail.com>
2017-06-04 11:16:38 -0500

    Project demonstrating the error.

---
+++ src/main/groovy/com/foo/main/GroovyClass.groovy
@@ -0,0 +1,5 @@
+package com.foo.main
+
+class GroovyClass {
+
+}
---
+++ src/main/java/com/foo/main/Main.java
@@ -0,0 +1,11 @@
+package com.foo.main;
+
+import com.foo.main.GroovyClass;
+
+public class Main {
+
+	public static void main(String[] args) {
+		GroovyClass gc;
+	}
+
+}
